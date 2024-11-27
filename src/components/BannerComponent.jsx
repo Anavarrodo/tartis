9e0f0ac3a73e1bd7cbb1341c3c5d2412ab5f1fea
Anavarrodo <A.navarrodo@gmail.com>
2024-11-27 05:49:18 +0100

    bannerComponent en movil

--- src/components/BannerComponent.jsx
+++ src/components/BannerComponent.jsx
@@ -1,64 +1,101 @@
 import React from 'react'
 import styled from 'styled-components'
 import bannerImage from '../assets/images/banner.svg'
+import bannerMobileImage from '../assets/images/bannerMobile.svg'
+import { sizes } from '../utils/breakpoints'
+import { useMediaQuery } from 'react-responsive'
 
-const BannerComponent = () => (
-  <Container>
-    <FirstLineTitle>Transformando ideas en</FirstLineTitle>
-    <SecondLineTitle>Soluciones Digitales</SecondLineTitle>
-    <FirstLineSubtitle>
-      Diseño e implementación de soluciones adaptadas
-    </FirstLineSubtitle>
-    <SecondLineSubtitle>para empresas de todos los tamaños</SecondLineSubtitle>
-  </Container>
-)
+const BannerComponent = () => {
+  const isMobile = useMediaQuery({ maxWidth: sizes.mobile })
+
+  return (
+    <Container>
+      <TextContainer>
+        <FirstLineTitle>Transformando ideas en</FirstLineTitle>
+        <SecondLineTitle>Soluciones Digitales</SecondLineTitle>
+        {!isMobile && (
+          <SubtitleContainer>
+            <FirstLineSubtitle>
+              Diseño e implementación de soluciones adaptadas para empresas de
+              todos los tamaños
+            </FirstLineSubtitle>
+          </SubtitleContainer>
+        )}
+      </TextContainer>
+    </Container>
+  )
+}
 
 export default BannerComponent
 
 const Container = styled.div`
-  width: 100%;
-  height: calc(100vh - 70px);
+  height: calc(100vh - 70px); /* Ajusta el tamaño del contenedor */
   background-image: url(${bannerImage});
   background-size: cover;
-  background-position: center;
+  background-position: center 20%; /* Mueve la imagen hacia abajo (ajustar el porcentaje según necesidad) */
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start; /* Alinea el contenido a la parte superior */
   margin-top: 70px;
+  padding-left: 150px; /* Espacio a la izquierda para evitar que el texto esté pegado al borde */
+
+  @media (max-width: ${sizes.mobile}px) {
+    background-image: url(${bannerMobileImage});
+    background-position: center 30%; /* Mueve la imagen más hacia abajo en pantallas pequeñas */
+    margin-top: 50px;
+    padding-left: 60px;
+    height: calc(100vh - 50px);
+  }
+`
+
+const TextContainer = styled.div`
+  color: #0b0f1d;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; /* Alineación hacia la parte superior */
+  align-items: flex-start;
+  max-width: 600px; /* Limita el ancho del texto para evitar que se vea muy largo en pantallas grandes */
+  padding-top: 180px; /* Ajusta este valor para mover el contenido más hacia abajo */
+  @media (max-width: ${sizes.mobile}px) {
+    padding-top: 345px;
+  }
 `
 
 const FirstLineTitle = styled.p`
-  position: absolute;
-  top: 200px;
-  left: 150px;
-  color: #0b0f1d;
   line-height: 38.4px;
   font-size: 32px;
+  color: #0b0f1d;
   font-family: 'fuenteRegular';
+  margin: 0;
+
+  @media (max-width: ${sizes.mobile}px) {
+    font-size: 16px;
+    line-height: 19.2px;
+  }
 `
+
 const SecondLineTitle = styled.p`
-  position: absolute;
-  top: 230px;
-  left: 150px;
   color: #0b0f1d;
   line-height: 76.8px;
   font-size: 64px;
   font-family: 'fuenteSemi';
+  margin: 0;
+
+  @media (max-width: ${sizes.mobile}px) {
+    font-size: 32px;
+    line-height: 38.4px;
+    margin: 14px 0px;
+  }
+`
+
+const SubtitleContainer = styled.div`
+  max-width: 525px;
 `
 
 const FirstLineSubtitle = styled.p`
-  position: absolute;
-  top: 380px;
-  left: 150px;
   color: #0b0f1d;
   line-height: 27.6px;
   font-size: 24px;
   font-family: 'fuenteRegular';
-`
-
-const SecondLineSubtitle = styled.p`
-  position: absolute;
-  top: 410px;
-  left: 150px;
-  color: #0b0f1d;
-  line-height: 27.6px;
-  font-size: 24px;
-  font-family: 'fuenteRegular';
+  margin-top: 20px; /* Elimina el margen superior */
 `
